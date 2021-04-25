b08e63a3ab7152ef5503c2a6bf9b4efe46ad15a6
Muriel Gasparini <muriel.developer@gmail.com>
2021-04-24 21:44:21 -0300

    set password to required

--- src/middlewares/request-body-validation/schemas/sign-up.ts
+++ src/middlewares/request-body-validation/schemas/sign-up.ts
@@ -6,7 +6,7 @@ const schema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(10).max(50).regex(emailRegex).required(),
   birthdate: Joi.date().required(),
-  password: Joi.string().min(5).max(50)
+  password: Joi.string().min(5).max(50).required()
 })
 
 export default schema
