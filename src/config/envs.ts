23aa4a81d2c8a1c85e41d28602452ef87cbc34fd
Muriel-Gasparini <muriel.developer@gmail.com>
2021-08-15 13:31:31 -0300

    create websocket server and set routes

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,11 @@ import { config } from 'dotenv'
 config()
 
 const variables = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   mongoUri: process.env.MONGO_URI || '',
   hashSalt: 12,
-  jwtSecret: process.env.JWT_SECRET || ''
+  jwtSecret: process.env.JWT_SECRET || '',
+  websocketCorsOrigin: process.env.WEBSOCKET_CORS_ORIGIN
 }
 
 export {variables}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,24 @@
-import Express from 'express'
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 
 import { variables } from '../config/envs'
 import { connectOnDatabase } from '../database/connection'
 import { setMiddlewares } from './middlewares/set-middlewares'
 import { setRoutes } from './routes'
+import { setChannels } from '../websocket/set-channels'
 
-const app = Express()
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: variables.websocketCorsOrigin
+  }
+})
 
 connectOnDatabase()
 setMiddlewares(app)
 setRoutes(app)
+setChannels(io)
 
-app.listen(variables.port, () => console.log(`Server Online on Port ${variables.port}`))
+server.listen(variables.port, () => console.log(`Server Online on Port ${variables.port}`))
