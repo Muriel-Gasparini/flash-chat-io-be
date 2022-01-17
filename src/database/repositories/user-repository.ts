f96882247e664f9e3734e667128b9fcb9af34407
lermen12 <lermenlucas@outlook.com>
2022-01-16 22:03:19 -0300

    refactor user repository errors

--- src/database/repositories/user-repository.ts
+++ src/database/repositories/user-repository.ts
@@ -3,62 +3,54 @@ import User, { IUser } from '../models/user'
 
 class UserRepository {
 
-  async create (data: IUser): Promise<void | Error | IUser> {
+  async create(data: IUser): Promise<void | Error | IUser> {
     try {
       return await User.create(data)
     } catch (error) {
-      if (error.toString().match(/duplicate/)) throw 'Email already exists'
-      throw new Error(error)
+      if (error instanceof Error && error.toString().match(/duplicate/)) throw 'Email already exists'
+      throw error
     }
   }
 
-  async findByEmail(email: string): Promise<IUser> {
-    try {
-      const user = await User.findOne({ email })
+  async findByEmail(email: string, selectPassword?: boolean): Promise<IUser> {
+    const user = !selectPassword ? await User.findOne({ email }) : await User.findOne({ email }).select('+password')
 
-      if (!user) {
-        throw new Error('This email does not exist')
-      }
-
-      return user
-    } catch (error) {
-      throw new Error(error)
+    if (!user) {
+      throw new Error('This email does not exist')
     }
+
+    return user
   }
 
-  async findById(id: string): Promise<IUser> {
-    try {
-      const user = await User.findOne({ _id: id })
+  async findById(id: string, selectPassword?: boolean): Promise<IUser | undefined> {
+    const user = !selectPassword ? await User.findOne({ _id: id }) : await User.findOne({ _id: id }).select('+password')
 
-      if (!user) {
-        throw new Error('This account does not exist')
-      }
-
-      return user
-    } catch (error) {
-      throw new Error(error)
+    if (!user) {
+      throw new Error('This account does not exist')
     }
+
+    return user
   }
 
-  async getUserByToken (headerAuthorization: string | undefined): Promise<IUser | undefined> {
+  async getUserByToken(headerAuthorization: string | undefined): Promise<IUser | undefined> {
     try {
       if (!headerAuthorization) {
         throw 'Invalid Token'
       }
-  
+
       const jwtService = createJwtService()
       const userRepository = new UserRepository()
-    
+
       const tokenPrefix = 'Bearer '
       const userToken = headerAuthorization?.replace(tokenPrefix, '') || ''
-    
+
       const { data } = await jwtService.getTokenPayload(userToken)
-      
+
       const userAccount = await userRepository.findById(data)
-      
+
       return userAccount
     } catch (error) {
-      throw new Error(error)
+      throw error
     }
   }
 }
